ecf8d2948c0f9ebf18b53d5539334790699a3a2e
hashi0203 <n.hsmt1123@gmail.com>
2022-05-28 12:15:10 +0900

    convlstm scale is wrong

---
+++ convlstm.py
@@ -0,0 +1,89 @@
+import numpy as np
+import nngen as ng
+
+
+class celu():
+    def __init__(self, xshift):
+        self.xshift = xshift
+
+    def __call__(self, x):
+        celu_table = [0, -2016, -3971, -5865, -7701, -9480, -11205, -12876, -14497, -16067, -17589, -19064, -20494, -21880, -23223, -24525, -25786, -27009, -28195, -29344, -30457, -31536, -32582, -33596, -34579, -35531, -36455, -37349, -38217, -39057, -39872, -40661, -41427, -42168, -42887, -43584, -44260, -44914, -45549, -46164, -46760, -47337, -47897, -48440, -48966, -49476, -49970, -50449, -50913, -51363, -51799, -52222, -52631, -53028, -53413, -53786, -54148, -54498, -54838, -55167, -55486, -55795, -56095, -56385, -56667, -56940, -57204, -57460, -57709, -57950, -58183, -58409, -58629, -58841, -59047, -59247, -59440, -59628, -59810, -59986, -60156, -60322, -60482, -60638, -60789, -60935, -61076, -61213, -61346, -61475, -61600, -61721, -61839, -61952, -62063, -62170, -62273, -62374, -62471, -62565, -62657, -62745, -62831, -62914, -62995, -63073, -63149, -63222, -63293, -63362, -63429, -63494, -63557, -63618, -63677, -63734, -63790, -63843, -63895, -63946, -63995, -64042, -64088, -64133, -64176, -64218, -64258, -64298, -64336, -64373, -64408, -64443, -64477, -64509, -64541, -64572, -64601, -64630, -64658, -64685, -64711, -64736, -64761, -64785, -64808, -64830, -64852, -64873, -64894, -64913, -64932, -64951, -64969, -64986, -65003, -65020, -65036, -65051, -65066, -65080, -65094, -65108, -65121, -65134, -65146, -65158, -65170, -65181, -65192, -65203, -65213, -65223, -65233, -65242, -65251, -65260, -65268, -65276, -65284, -65292, -65300, -65307, -65314, -65321, -65327, -65334, -65340, -65346, -65352, -65358, -65363, -65368, -65374, -65379, -65383, -65388, -65393, -65397, -65401, -65405, -65409, -65413, -65417, -65421, -65424, -65428, -65431, -65434, -65437, -65441, -65443, -65446, -65449, -65452, -65454, -65457, -65459, -65462, -65464, -65466, -65468, -65470, -65472, -65474, -65476, -65478, -65480, -65482, -65483, -65485, -65486, -65488, -65489, -65491, -65492, -65494, -65495, -65496, -65497, -65499, -65500, -65501, -65502, -65503, -65504, -65505, -65506, -65507, -65508, -65509, -65509, -65510, -65511, -65512, -65513, -65513]
+        tbbit = 8
+        tbshift = 5
+        celushift = 16
+        xshift = self.xshift
+
+        for i in range(x.shape[0]):
+            for j in range(x.shape[1]):
+                for k in range(x.shape[2]):
+                    for l in range(x.shape[3]):
+                        tb_idx = (-x[i][j][k][l]) >> (xshift - tbshift)
+                        if x[i][j][k][l] > 0:
+                            x[i][j][k][l] = x[i][j][k][l] << (celushift - xshift)
+                        elif tb_idx >= (1 << tbbit):
+                            x[i][j][k][l] = -1 << celushift
+                        else:
+                            x[i][j][k][l] = celu_table[tb_idx]
+
+        return x
+
+
+class ln():
+    def __init__(self, lshift):
+        self.lshift = lshift
+
+    def __call__(self, x):
+        eps = 1e-5
+        e = np.mean(x.reshape(-1, x.shape[-1]), axis=0)
+        v = np.var(x.reshape(-1, x.shape[-1]), axis=0)
+        return np.round((x - e) / np.sqrt(v + eps) * (1 << self.lshift)).astype(x.dtype)
+
+
+def LSTMFusion(act100, act101, act102, params,
+               weight_dtype=ng.int8, bias_dtype=ng.int32, scale_dtype=ng.int8, act_dtype=ng.int32):
+
+    # [103] cat
+    act103 = ng.concat([act100, act101], axis=3)
+
+
+    # [104] conv
+    weight104 = ng.variable(dtype=weight_dtype, shape=(2048, 3, 3, 1024), name="lstm_cell.conv.weight")
+    weight104.set_value(params["lstm_cell.conv.weight"])
+
+    # bias104 = ng.variable(dtype=bias_dtype, shape=(2048,), name="lstm_cell.conv.bias")
+    # bias104.set_value(params["lstm_cell.conv.bias"])
+
+    conv104 = ng.conv2d(act103, weight104, strides=(1, 1, 1, 1), dtype=act_dtype, sum_dtype=ng.int32)
+
+    # lshift104 = ng.constant([20], dtype=ng.int8)
+    # sum104 = ng.add(conv104, ng.lshift(bias104, lshift104))
+    rshift104 = ng.constant([11], dtype=ng.int8)
+    act104 = ng.rshift_round(conv104, rshift104)
+
+
+    # [105] sig_ln_celu
+    slice105s = [ng.slice_(act104, (0, 0, 0, i * 512), (1, 2, 3, (i+1) * 512), (1, 1, 1, 1)) for i in range(4)]
+
+    rshift105 = ng.constant([4], dtype=ng.int8)
+    ii105, ff105, oo105 = [ng.sigmoid(ng.rshift_round(slice105s[i], rshift105), lut_addrwidth=9, lut_clip=8.0, range_rate=1.0) for i in range(3)]
+
+    gg105 = ng.extern([slice105s[3]], opcode=0x105, func=lambda x : celu(13)(ln(13)(x)))
+
+
+    # [106] cell_state
+    in_rshift106 = ng.constant([4], dtype=ng.int8)
+    out_rshift106 = ng.constant([12], dtype=ng.int8)
+    sum106 = ng.rshift_round(ng.add(ng.multiply(ng.rshift_round(ff105, in_rshift106), act102), ng.multiply(ng.rshift_round(ii105, in_rshift106), ng.rshift_round(gg105, in_rshift106))), out_rshift106)
+    act106 = ng.extern([sum106], opcode=0x106, func=ln(12))
+
+
+    # [107] hidden_state
+    celu107 = ng.extern([act106], opcode=0x107, func=celu(12))
+    in_rshift107 = ng.constant([1], dtype=ng.int8)
+    rshift107 = ng.constant([16], dtype=ng.int8)
+    act107 = ng.rshift_round(ng.multiply(ng.rshift_round(celu107, in_rshift107), ng.rshift_round(oo105, in_rshift107)), rshift107)
+
+
+    return act107, act106
+
+
--- dvmvs.py
+++ dvmvs.py
@@ -11,6 +11,7 @@ from feature_extractor import feature_extractor
 from feature_shrinker import feature_shrinker
 from cost_volume_fusion import cost_volume_fusion
 from cost_volume_encoder import cost_volume_encoder
+from convlstm import LSTMFusion
 
 act_dtype = ng.int32
 weight_dtype = ng.int8
@@ -26,34 +27,34 @@ outputs = np.load(os.path.join(base_dir, "params/outputs.npz"))
 
 # print(inputs["measurement_features"][0][0][0])
 
-input_scale_factor = 1 << 12
-input_layer_value = inputs["input"].transpose(0, 2, 3, 1)
-input_layer_value = input_layer_value * input_scale_factor
-input_layer_value = np.clip(input_layer_value,
-                            -1 * 2 ** (act_dtype.width - 1) - 1, 2 ** (act_dtype.width - 1))
-input_layer_value = np.round(input_layer_value.astype(np.float64)).astype(np.int32)
+def prepare_input_value(value, lshift):
+    value *= 1 << lshift
+    value = np.clip(value, -1 * 2 ** (act_dtype.width - 1) - 1, 2 ** (act_dtype.width - 1))
+    return np.round(value.astype(np.float64)).astype(np.int32)
 
-features_scale_factor = 1 << 9
-measurement_features_value = inputs["measurement_features"].transpose(0, 1, 3, 4, 2)
-measurement_features_value = measurement_features_value * features_scale_factor
-measurement_features_value = np.clip(measurement_features_value,
-                                     -1 * 2 ** (act_dtype.width - 1) - 1, 2 ** (act_dtype.width - 1))
-measurement_features_value = np.round(measurement_features_value.astype(np.float64)).astype(np.int32)
+
+input_layer_value = prepare_input_value(inputs["input"].transpose(0, 2, 3, 1), 12)
+measurement_features_value = prepare_input_value(inputs["measurement_features"].transpose(0, 1, 3, 4, 2), 9)
+hidden_state_value = prepare_input_value(inputs["hidden_state"].transpose(0, 2, 3, 1), 14-1)
+cell_state_value = prepare_input_value(inputs["cell_state"].transpose(0, 2, 3, 1), 12)
 
 n_measurement_frames = measurement_features_value.shape[0]
 
-# # input
+
+# input
 print("preparing inputs...")
-input_layer = ng.placeholder(dtype=act_dtype,
-                             shape=(batchsize, 64, 96, 3),  # N, H, W, C
-                             name='input_layer')
+input_layer = ng.placeholder(dtype=act_dtype, shape=(batchsize, 64, 96, 3), name='input_layer')
 measurement_features = [ng.placeholder(dtype=act_dtype, shape=(batchsize, 32, 48, 32), name='measurement_feature%d' % m)
                         for m in range(n_measurement_frames)]
+hidden_state = ng.placeholder(dtype=act_dtype, shape=(batchsize, 2, 3, 512), name='hidden_state')
+cell_state = ng.placeholder(dtype=act_dtype, shape=(batchsize, 2, 3, 512), name='cell_state')
 
 ng_inputs = {}
 ng_inputs["input_layer"] = input_layer_value
 for m in range(n_measurement_frames):
     ng_inputs["measurement_feature%d" % m] = measurement_features_value[m]
+ng_inputs["hidden_state"] = hidden_state_value
+ng_inputs["cell_state"] = cell_state_value
 
 
 print("preparing feature extractor...")
@@ -68,18 +69,23 @@ cost_volume = cost_volume_fusion(reference_features[0], measurement_features, in
 print("preparing cost volume encoder...")
 skips = cost_volume_encoder(*reference_features, cost_volume, params)
 
+print("preparing LSTM fusion...")
+lstm_states = LSTMFusion(skips[-1], hidden_state, cell_state, params)
+
 print("evaluating...")
-eval_outs = ng.eval(layers + reference_features[::-1] + (cost_volume,) + skips, **ng_inputs)
+eval_outs = ng.eval(layers + reference_features[::-1] + (cost_volume,) + skips + lstm_states[::-1], **ng_inputs)
 
 
 files = ["layer1", "layer2", "layer3", "layer4", "layer5",
          "feature_one_sixteen", "feature_one_eight", "feature_quarter", "feature_half",
          "cost_volume",
-         "skip0", "skip1", "skip2", "skip3", "bottom"]
+         "skip0", "skip1", "skip2", "skip3", "bottom",
+         "cell_state", "hidden_state"]
 shifts = [11, 11, 11, 12, 13,
           11, 11, 10, 9,
           7,
-          13, 13, 13, 12, 13]
+          13, 13, 13, 12, 13,
+          12, 14]
 for i in range(len(eval_outs)):
     print(files[i], outputs[files[i]].shape)
     output_layer_value = eval_outs[i].transpose(0, 3, 1, 2) / (1 << shifts[i])
