b61aac8265602071d302c95597a7345ffaaf4dab
hashi0203 <n.hsmt1123@gmail.com>
2022-05-30 19:34:11 +0900

    apply to sequencial processing, but the result is a bit different (should be same). corrcoef is 0.963

--- cost_volume_fusion.py
+++ cost_volume_fusion.py
@@ -1,5 +1,6 @@
 import numpy as np
 import nngen as ng
+import torch
 from utils import round_and_clip
 
 n_depth_levels = 64
@@ -32,7 +33,6 @@ def calculate_cost_volume_by_warping(image1, image2, warping, out_shape):
 
     for depth_i in range(n_depth_levels):
         # warped_image2 = grid_sample(image2, warping[depth_i][0])
-        import torch
         warped_image2 = torch.nn.functional.grid_sample(input=torch.tensor(image2.astype(np.float32).transpose(0, 3, 1, 2)),
                                                         grid=torch.tensor(warping[depth_i]),
                                                         mode='bilinear',
@@ -45,27 +45,86 @@ def calculate_cost_volume_by_warping(image1, image2, warping, out_shape):
 
 
 class fusion():
-    def __init__(self, rshift, warpings, n_measurement_frames):
+    def __init__(self, rshift, K, pose1s, pose2ss):
         self.rshift = rshift
-        self.warpings = warpings
-        self.n_measurement_frames = n_measurement_frames
+        self.K = torch.tensor(K)
+        self.pose1s = torch.tensor(pose1s)
+        self.pose2ss = torch.tensor(pose2ss)
 
-    def __call__(self, image1, *image2s):
+        test_image_width = 96
+        test_image_height = 64
+        self.warp_grid = self.get_warp_grid_for_cost_volume_calculation(int(test_image_width / 2), int(test_image_height / 2))
+
+
+    def get_warp_grid_for_cost_volume_calculation(self, width, height):
+        x = np.linspace(0, width - 1, num=int(width))
+        y = np.linspace(0, height - 1, num=int(height))
+        ones = np.ones(shape=(height, width))
+        x_grid, y_grid = np.meshgrid(x, y)
+        warp_grid = np.stack((x_grid, y_grid, ones), axis=-1)
+        warp_grid = torch.from_numpy(warp_grid).float()
+        warp_grid = warp_grid.view(-1, 3).t()
+        return warp_grid
+
+
+    def calc_warpings(self, frame_number, batchsize, height, width, n_measurement_frames):
+        K = self.K
+        pose1 = self.pose1s[frame_number]
+        pose2s = self.pose2ss[frame_number]
+        warp_grid = self.warp_grid
+
+        min_depth = 0.25
+        max_depth = 20.0
+
+        warp_grid = torch.cat(batchsize * [warp_grid.unsqueeze(dim=0)])
+
+        warpings = [[] for _ in range(n_measurement_frames)]
+        for m in range(n_measurement_frames):
+            pose2 = pose2s[m]
+
+            extrinsic2 = torch.inverse(pose2).bmm(pose1)
+            R = extrinsic2[:, 0:3, 0:3]
+            t = extrinsic2[:, 0:3, 3].unsqueeze(-1)
+
+            Kt = K.bmm(t)
+            K_R_Kinv = K.bmm(R).bmm(torch.inverse(K))
+            K_R_Kinv_UV = K_R_Kinv.bmm(warp_grid)
+
+            inverse_depth_base = 1.0 / max_depth
+            inverse_depth_step = (1.0 / min_depth - 1.0 / max_depth) / (n_depth_levels - 1)
+
+            width_normalizer = width / 2.0
+            height_normalizer = height / 2.0
+
+            for depth_i in range(n_depth_levels):
+                this_depth = 1 / (inverse_depth_base + depth_i * inverse_depth_step)
+
+                warping = K_R_Kinv_UV + (Kt / this_depth)
+                warping = warping.transpose(dim0=1, dim1=2)
+                warping = warping[:, :, 0:2] / (warping[:, :, 2].unsqueeze(-1) + 1e-8)
+                warping = warping.view(batchsize, height, width, 2)
+                warping[:, :, :, 0] = (warping[:, :, :, 0] - width_normalizer) / width_normalizer
+                warping[:, :, :, 1] = (warping[:, :, :, 1] - height_normalizer) / height_normalizer
+                warpings[m].append(warping.cpu().detach().numpy().copy())
+
+        return np.array(warpings)
+
+
+    def __call__(self, frame_number, image1, n_measurement_frames, *image2s):
         rshift = self.rshift
-        warpings = self.warpings
-        n_measurement_frames = self.n_measurement_frames
 
         batchsize, height, width, _ = image1.shape
         out_shape = (batchsize, height, width, n_depth_levels)
         fused_cost_volume = np.zeros(out_shape)
-        for m in range(n_measurement_frames):
+        warpings = self.calc_warpings(frame_number[0], batchsize, height, width, n_measurement_frames[0])
+        for m in range(n_measurement_frames[0]):
             fused_cost_volume += calculate_cost_volume_by_warping(image1, image2s[m], warpings[m], out_shape)
 
         return round_and_clip((fused_cost_volume / n_measurement_frames) / (1 << rshift), image1.dtype)
 
 
-def cost_volume_fusion(act78, act79s, warpings, n_measurement_frames=2):
+def cost_volume_fusion(frame_number, act78, n_measurement_frames, act79s, K, pose1s, pose2ss, act_dtype=ng.int16):
     # [80] conv
-    act80 = ng.extern([act78, *act79s], shape=(1, 32, 48, 64), opcode=0x80, func=fusion(11, warpings, n_measurement_frames))
+    act80 = ng.extern([frame_number, act78, n_measurement_frames, *act79s], shape=(1, 32, 48, 64), dtype=act_dtype, opcode=0x80, func=fusion(11, K, pose1s, pose2ss))
 
     return act80
--- dvmvs.py
+++ dvmvs.py
@@ -19,6 +19,7 @@ bias_dtype = ng.int32
 scale_dtype = ng.int8
 act_dtype = ng.int16
 batchsize = 1
+max_n_measurement_frames = 2
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 params = np.load(os.path.join(base_dir, "params/params.npz"))
@@ -26,44 +27,18 @@ inputs = np.load(os.path.join(base_dir, "params/inputs.npz"))
 outputs = np.load(os.path.join(base_dir, "params/outputs.npz"))
 # mids = np.load(os.path.join(base_dir, "params/mids.npz"))
 
-
-def prepare_input_value(value, lshift):
-    value *= 1 << lshift
-    value = np.clip(value, -1 * 2 ** (act_dtype.width - 1) - 1, 2 ** (act_dtype.width - 1))
-    return np.round(value.astype(np.float64)).astype(np.int32)
-
-
-input_layer_value = prepare_input_value(inputs["input"].transpose(0, 2, 3, 1), 12)
-measurement_features_value = prepare_input_value(inputs["measurement_features"].transpose(0, 1, 3, 4, 2), 9)
-hidden_state_value = prepare_input_value(inputs["hidden_state"].transpose(0, 2, 3, 1), 14-1)
-cell_state_value = prepare_input_value(inputs["cell_state"].transpose(0, 2, 3, 1), 12)
-
-n_measurement_frames = measurement_features_value.shape[0]
-
-
 # input
 print("preparing inputs...")
 input_layer = ng.placeholder(dtype=act_dtype, shape=(batchsize, 64, 96, 3), name='input_layer')
 measurement_features = [ng.placeholder(dtype=act_dtype, shape=(batchsize, 32, 48, 32), name='measurement_feature%d' % m)
-                        for m in range(n_measurement_frames)]
+                        for m in range(max_n_measurement_frames)]
+n_measurement_frames = ng.placeholder(dtype=ng.uint8, shape=(1,), name='n_measurement_frames')
+frame_number = ng.placeholder(dtype=ng.uint8, shape=(1,), name='frame_number')
 hidden_state = ng.placeholder(dtype=act_dtype, shape=(batchsize, 2, 3, 512), name='hidden_state')
 cell_state = ng.placeholder(dtype=act_dtype, shape=(batchsize, 2, 3, 512), name='cell_state')
 
-ng_inputs = {}
-ng_inputs["input_layer"] = input_layer_value
-for m in range(n_measurement_frames):
-    ng_inputs["measurement_feature%d" % m] = measurement_features_value[m]
-ng_inputs["hidden_state"] = hidden_state_value
-ng_inputs["cell_state"] = cell_state_value
-
-
 # feature_list = ["half", "quarter", "one_eight", "one_sixteen"]
-# feature_shifts = [9, 10, 11, 11]
-# features_value = [prepare_input_value(outputs["feature_%s" % feature_list[i]].transpose(0, 2, 3, 1), feature_shifts[i]) for i in range(4)]
-# reference_features = [ng.placeholder(dtype=act_dtype, shape=features_value[i].shape, name='feature_%s' % feature_list[i]) for i in range(4)]
-# for i in range(4):
-#     ng_inputs["feature_%s" % feature_list[i]] = features_value[i]
-
+# reference_features = [ng.placeholder(dtype=act_dtype, shape=(batchsize, 32 >> i, 48 >> i, 32), name='feature_%s' % feature_list[i]) for i in range(4)]
 
 print("preparing feature extractor...")
 layers = feature_extractor(input_layer, params)
@@ -72,7 +47,8 @@ print("preparing feature shrinker...")
 reference_features = feature_shrinker(*layers, params)
 
 print("preparing cost volume fusion...")
-cost_volume = cost_volume_fusion(reference_features[0], measurement_features, inputs["warpings"], n_measurement_frames)
+cost_volume = cost_volume_fusion(frame_number, reference_features[0], n_measurement_frames, measurement_features,
+                                 inputs["K"], inputs["pose1s"], inputs["pose2ss"])
 
 print("preparing cost volume encoder...")
 skips = cost_volume_encoder(*reference_features, cost_volume, params)
@@ -83,29 +59,57 @@ lstm_states = LSTMFusion(skips[-1], hidden_state, cell_state, params)
 print("preparing cost volume decoder...")
 depth_full = cost_volume_decoder(input_layer, *skips[:-1], lstm_states[0], params)
 
-print("evaluating...")
-eval_outs = ng.eval(layers + reference_features[::-1] + (cost_volume,) + skips + lstm_states[::-1] + (depth_full,), **ng_inputs)
-# eval_outs = ng.eval((cost_volume,) + skips + lstm_states[::-1] + (depth_full,), **ng_inputs)
+
+def prepare_input_value(value, lshift):
+    value *= 1 << lshift
+    value = np.clip(value, -1 * 2 ** (act_dtype.width - 1) - 1, 2 ** (act_dtype.width - 1))
+    return np.round(value.astype(np.float64)).astype(np.int32)
+
+for n in range(1):
+    input_layer_value = prepare_input_value(inputs["input"].transpose(0, 2, 3, 1), 12)
+    measurement_features_value = prepare_input_value(inputs["measurement_features"].transpose(0, 1, 3, 4, 2), 9)
+    n_measurement_frames_value = np.array([inputs["n_measurement_frames"]]).astype(np.int8)
+    frame_number_value = np.array([n]).astype(np.int8)
+    hidden_state_value = prepare_input_value(inputs["hidden_state"].transpose(0, 2, 3, 1), 14-1)
+    cell_state_value = prepare_input_value(inputs["cell_state"].transpose(0, 2, 3, 1), 12)
+
+    ng_inputs = {}
+    ng_inputs["input_layer"] = input_layer_value
+    for m in range(max_n_measurement_frames):
+        ng_inputs["measurement_feature%d" % m] = measurement_features_value[m]
+    ng_inputs["n_measurement_frames"] = n_measurement_frames_value
+    ng_inputs["frame_number"] = frame_number_value
+    ng_inputs["hidden_state"] = hidden_state_value
+    ng_inputs["cell_state"] = cell_state_value
+
+    # feature_shifts = [9, 10, 11, 11]
+    # features_value = [prepare_input_value(outputs["feature_%s" % feature_list[i]].transpose(0, 2, 3, 1), feature_shifts[i]) for i in range(4)]
+    # for i in range(4):
+    #     ng_inputs["feature_%s" % feature_list[i]] = features_value[i]
 
 
-files = ["layer1", "layer2", "layer3", "layer4", "layer5",
-         "feature_one_sixteen", "feature_one_eight", "feature_quarter", "feature_half",
-         "cost_volume",
-         "skip0", "skip1", "skip2", "skip3", "bottom",
-         "cell_state", "hidden_state",
-         "depth_org"]
-shifts = [11, 11, 11, 12, 13,
-          11, 11, 10, 9,
-          7,
-          13, 13, 13, 12, 13,
-          12, 14,
-          14]
-# files = files[9:]
-# shifts = shifts[9:]
-for i in range(len(eval_outs)):
-    print(files[i], outputs[files[i]].shape)
-    output_layer_value = eval_outs[i].transpose(0, 3, 1, 2) / (1 << shifts[i])
-    print(np.mean(output_layer_value.reshape(-1)), np.std(output_layer_value.reshape(-1)))
-    print(np.mean(outputs[files[i]].reshape(-1)), np.std(outputs[files[i]].reshape(-1)))
-    print(np.corrcoef(output_layer_value.reshape(-1), outputs[files[i]].reshape(-1))[0, 1])
-    print("--------------------------")
+    print("evaluating...")
+    eval_outs = ng.eval(layers + reference_features[::-1] + (cost_volume,) + skips + lstm_states[::-1] + (depth_full,), **ng_inputs)
+    # eval_outs = ng.eval((cost_volume,) + skips + lstm_states[::-1] + (depth_full,), **ng_inputs)
+
+    files = ["layer1", "layer2", "layer3", "layer4", "layer5",
+            "feature_one_sixteen", "feature_one_eight", "feature_quarter", "feature_half",
+            "cost_volume",
+            "skip0", "skip1", "skip2", "skip3", "bottom",
+            "cell_state", "hidden_state",
+            "depth_org"]
+    shifts = [11, 11, 11, 12, 13,
+            11, 11, 10, 9,
+            7,
+            13, 13, 13, 12, 13,
+            12, 14,
+            14]
+    # files = files[9:]
+    # shifts = shifts[9:]
+    for i in range(len(eval_outs)):
+        print(files[i], outputs[files[i]].shape)
+        output_layer_value = eval_outs[i].transpose(0, 3, 1, 2) / (1 << shifts[i])
+        print(np.mean(output_layer_value.reshape(-1)), np.std(output_layer_value.reshape(-1)))
+        print(np.mean(outputs[files[i]].reshape(-1)), np.std(outputs[files[i]].reshape(-1)))
+        print(np.corrcoef(output_layer_value.reshape(-1), outputs[files[i]].reshape(-1))[0, 1])
+        print("--------------------------")
