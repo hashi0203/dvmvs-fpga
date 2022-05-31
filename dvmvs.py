478dabe40bcbef131620b410707443d93e2e9e84
hashi0203 <n.hsmt1123@gmail.com>
2022-05-31 23:02:06 +0900

    remove unnecessary imports

--- dvmvs.py
+++ dvmvs.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 from __future__ import print_function
-from logging import PlaceHolder
 
-import sys
 import os
 from xml.sax.handler import feature_external_ges
 
