c820b134e6eba727f60bc75c105ae0cfe6959f05
hashi0203 <n.hsmt1123@gmail.com>
2022-06-06 18:05:32 +0900

    accelerate fpga execution by soft-hard parallelism

--- pynq/extern_funcs.py
+++ pynq/extern_funcs.py
@@ -26,47 +26,8 @@ class interpolate():
         else:
             out_shape = (out_height, out_width)
             mid = torch.tensor(input.astype(np.float32).transpose(0, 3, 1, 2))
-            if mode == "nearest":
-                mid = torch.nn.functional.interpolate(mid, size=out_shape, mode="nearest")
-            elif mode == "bilinear":
-                mid = torch.nn.functional.interpolate(mid, size=out_shape, mode='bilinear', align_corners=True)
+            mid = torch.nn.functional.interpolate(mid, size=out_shape, mode=mode, align_corners=True)
             output = mid.detach().numpy().copy().transpose(0, 2, 3, 1).astype(input.dtype)
-
-        # if mode == "nearest":
-        #     fy = in_height / out_height
-        #     fx = in_width / out_width
-        #     for j in range(out_height):
-        #         for k in range(out_width):
-        #             y = int(j * fy)
-        #             x = int(k * fx)
-        #             for i in range(channels):
-        #                 # input_idx = (i * in_height + y) * in_width + x
-        #                 # output_idx = (i * out_height + j) * out_width + k
-        #                 output[0][j][k][i] = input[0][y][x][i]
-        # elif mode == "bilinear":
-        #     if in_height < out_height:
-        #         fy = (in_height - 1) / (out_height - 1)
-        #         fx = (in_width - 1) / (out_width - 1)
-        #         for j in range(out_height):
-        #             for k in range(out_width):
-        #                 y = j * fy
-        #                 x = k * fx
-        #                 y_int = int(y) if y < in_height-1 else int(y) - 1
-        #                 x_int = int(x) if x < in_width-1 else int(x) - 1
-        #                 ys = [y_int, y_int + 1]
-        #                 xs = [x_int, x_int + 1]
-        #                 dys = [y - ys[0], ys[1] - y]
-        #                 dxs = [x - xs[0], xs[1] - x]
-        #                 for i in range(channels):
-        #                     sum = 0
-        #                     for yi in range(2):
-        #                         for xi in range(2):
-        #                             sum += dys[1-yi] * dxs[1-xi] * input[0][ys[yi]][xs[xi]][i]
-        #                     output[0][j][k][i] = int(round(sum)) >> rshift
-        #     else:
-        #         print("in_height is larger than out_height")
-        # else:
-        #     print("The 'mode' option in interpolation should be 'nearest' or 'bilinear,' but it is", mode)
 
         return output
 
@@ -246,30 +207,6 @@ class lstm_state_calculator():
         return prepare_input_value(h_cur.detach().numpy().copy().transpose(0, 2, 3, 1), hshift), c_cur
 
 
-
-# def grid_sample(image, warping):
-#     _, height, width, channels = image.shape
-#     warped_image = np.zeros(image.shape)
-
-#     for j in range(height):
-#         for k in range(width):
-#             x = (warping[j][k][0] + 1) * (width - 1) / 2.0
-#             y = (warping[j][k][1] + 1) * (height - 1) / 2.0
-#             y_int = int(y)
-#             x_int = int(x)
-#             ys = [y_int, y_int + 1]
-#             xs = [x_int, x_int + 1]
-#             dys = [y - ys[0], ys[1] - y]
-#             dxs = [x - xs[0], xs[1] - x]
-#             for i in range(channels):
-#                 for yi in range(2):
-#                     for xi in range(2):
-#                         if not(ys[yi] < 0 or height-1 < ys[yi] or xs[xi] < 0 or width-1 < xs[xi]):
-#                             warped_image[0][j][k][i] += dys[1-yi] * dxs[1-xi] * image[0][ys[yi]][xs[xi]][i]
-
-#     return warped_image
-
-
 class fusion():
     n_depth_levels = 64
 
@@ -339,63 +276,40 @@ class fusion():
         return np.array(warpings)
 
 
-    def calculate_cost_volume_by_warping(self, image1, image2, warping, out_shape):
+    def prep(self, frame_number, n_measurement_frames, image2s):
+        self.n_measurement_frames = n_measurement_frames
         n_depth_levels = self.n_depth_levels
-        cost_volume = np.zeros(out_shape)
 
-        for depth_i in range(n_depth_levels):
-            # warped_image2 = grid_sample(image2, warping[depth_i][0])
-            warped_image2 = torch.nn.functional.grid_sample(input=torch.tensor(image2.astype(np.float32).transpose(0, 3, 1, 2)),
-                                                            grid=torch.tensor(warping[depth_i]),
-                                                            mode='bilinear',
-                                                            padding_mode='zeros',
-                                                            align_corners=True)
-            warped_image2 = warped_image2.detach().numpy().copy().transpose(0, 2, 3, 1)
-            cost_volume[:,:,:,depth_i] = np.sum(image1 * warped_image2, axis=3) / image1.shape[-1]
+        batchsize, height, width, channels = image2s[0].shape
+        warpings = self.calc_warpings(frame_number[0], batchsize, height, width, n_measurement_frames[0])
 
-        return cost_volume
+        self.warped_image2s = [[] for _ in range(n_measurement_frames[0])]
+        for m in range(n_measurement_frames[0]):
+            warping = warpings[m]
+            image2 = image2s[m]
+            for depth_i in range(n_depth_levels):
+                # warped_image2 = grid_sample(image2, warping[depth_i][0])
+                warped_image2 = torch.nn.functional.grid_sample(input=torch.tensor(image2.astype(np.float32).transpose(0, 3, 1, 2)),
+                                                                grid=torch.tensor(warping[depth_i]),
+                                                                mode='bilinear',
+                                                                padding_mode='zeros',
+                                                                align_corners=True)
+                self.warped_image2s[m].append(warped_image2.detach().numpy().copy().transpose(0, 2, 3, 1) / channels)
 
 
-    def __call__(self, frame_number, image1, n_measurement_frames, *image2s):
+    def __call__(self, image1):
         n_depth_levels = self.n_depth_levels
+        n_measurement_frames = self.n_measurement_frames
         rshift = self.rshift
 
         batchsize, height, width, _ = image1.shape
-        out_shape = (batchsize, height, width, n_depth_levels)
-        fused_cost_volume = np.zeros(out_shape)
-        warpings = self.calc_warpings(frame_number[0], batchsize, height, width, n_measurement_frames[0])
+        fused_cost_volume = np.zeros((batchsize, height, width, n_depth_levels))
         for m in range(n_measurement_frames[0]):
-            fused_cost_volume += self.calculate_cost_volume_by_warping(image1, image2s[m], warpings[m], out_shape)
+            for depth_i in range(n_depth_levels):
+                warped_image2 = self.warped_image2s[m][depth_i]
+                fused_cost_volume[:,:,:,depth_i] += np.sum(image1 * warped_image2, axis=3)
 
         return round_and_clip((fused_cost_volume / n_measurement_frames) / (1 << rshift), image1.dtype)
-
-
-
-class celu():
-    def __init__(self, xshift):
-        self.xshift = xshift
-
-    def __call__(self, x):
-        # celu_table = [0, -2016, -3971, -5865, -7701, -9480, -11205, -12876, -14497, -16067, -17589, -19064, -20494, -21880, -23223, -24525, -25786, -27009, -28195, -29344, -30457, -31536, -32582, -33596, -34579, -35531, -36455, -37349, -38217, -39057, -39872, -40661, -41427, -42168, -42887, -43584, -44260, -44914, -45549, -46164, -46760, -47337, -47897, -48440, -48966, -49476, -49970, -50449, -50913, -51363, -51799, -52222, -52631, -53028, -53413, -53786, -54148, -54498, -54838, -55167, -55486, -55795, -56095, -56385, -56667, -56940, -57204, -57460, -57709, -57950, -58183, -58409, -58629, -58841, -59047, -59247, -59440, -59628, -59810, -59986, -60156, -60322, -60482, -60638, -60789, -60935, -61076, -61213, -61346, -61475, -61600, -61721, -61839, -61952, -62063, -62170, -62273, -62374, -62471, -62565, -62657, -62745, -62831, -62914, -62995, -63073, -63149, -63222, -63293, -63362, -63429, -63494, -63557, -63618, -63677, -63734, -63790, -63843, -63895, -63946, -63995, -64042, -64088, -64133, -64176, -64218, -64258, -64298, -64336, -64373, -64408, -64443, -64477, -64509, -64541, -64572, -64601, -64630, -64658, -64685, -64711, -64736, -64761, -64785, -64808, -64830, -64852, -64873, -64894, -64913, -64932, -64951, -64969, -64986, -65003, -65020, -65036, -65051, -65066, -65080, -65094, -65108, -65121, -65134, -65146, -65158, -65170, -65181, -65192, -65203, -65213, -65223, -65233, -65242, -65251, -65260, -65268, -65276, -65284, -65292, -65300, -65307, -65314, -65321, -65327, -65334, -65340, -65346, -65352, -65358, -65363, -65368, -65374, -65379, -65383, -65388, -65393, -65397, -65401, -65405, -65409, -65413, -65417, -65421, -65424, -65428, -65431, -65434, -65437, -65441, -65443, -65446, -65449, -65452, -65454, -65457, -65459, -65462, -65464, -65466, -65468, -65470, -65472, -65474, -65476, -65478, -65480, -65482, -65483, -65485, -65486, -65488, -65489, -65491, -65492, -65494, -65495, -65496, -65497, -65499, -65500, -65501, -65502, -65503, -65504, -65505, -65506, -65507, -65508, -65509, -65509, -65510, -65511, -65512, -65513, -65513]
-        celu_table = [0, -126, -248, -367, -481, -593, -700, -805, -906, -1004, -1099, -1192, -1281, -1367, -1451, -1533, -1612, -1688, -1762, -1834, -1904, -1971, -2036, -2100, -2161, -2221, -2278, -2334, -2389, -2441, -2492, -2541, -2589, -2636, -2680, -2724, -2766, -2807, -2847, -2885, -2922, -2959, -2994, -3027, -3060, -3092, -3123, -3153, -3182, -3210, -3237, -3264, -3289, -3314, -3338, -3362, -3384, -3406, -3427, -3448, -3468, -3487, -3506, -3524, -3542, -3559, -3575, -3591, -3607, -3622, -3636, -3651, -3664, -3678, -3690, -3703, -3715, -3727, -3738, -3749, -3760, -3770, -3780, -3790, -3799, -3808, -3817, -3826, -3834, -3842, -3850, -3858, -3865, -3872, -3879, -3886, -3892, -3898, -3904, -3910, -3916, -3922, -3927, -3932, -3937, -3942, -3947, -3951, -3956, -3960, -3964, -3968, -3972, -3976, -3980, -3983, -3987, -3990, -3993, -3997, -4000, -4003, -4006, -4008, -4011, -4014, -4016, -4019, -4021, -4023, -4026, -4028, -4030, -4032, -4034, -4036, -4038, -4039, -4041, -4043, -4044, -4046, -4048, -4049, -4050, -4052, -4053, -4055, -4056, -4057, -4058, -4059, -4061, -4062, -4063, -4064, -4065, -4066, -4067, -4068, -4068, -4069, -4070, -4071, -4072, -4072, -4073, -4074, -4075, -4075, -4076, -4076, -4077, -4078, -4078, -4079, -4079, -4080, -4080, -4081, -4081, -4082, -4082, -4083, -4083, -4083, -4084, -4084, -4084, -4085, -4085, -4086, -4086, -4086, -4086, -4087, -4087, -4087, -4088, -4088, -4088, -4088, -4089, -4089, -4089, -4089, -4089, -4090, -4090, -4090, -4090, -4090, -4091, -4091, -4091, -4091, -4091, -4091, -4091, -4092, -4092, -4092, -4092, -4092, -4092, -4092, -4092, -4093, -4093, -4093, -4093, -4093, -4093, -4093, -4093, -4093, -4093, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4094, -4095, -4095]
-        tbbit = 8
-        tbshift = 5
-        celushift = 12
-        xshift = self.xshift
-
-        for i in range(x.shape[0]):
-            for j in range(x.shape[1]):
-                for k in range(x.shape[2]):
-                    for l in range(x.shape[3]):
-                        tb_idx = (-x[i][j][k][l]) >> (xshift - tbshift)
-                        if x[i][j][k][l] > 0:
-                            x[i][j][k][l] = x[i][j][k][l]
-                        elif tb_idx >= (1 << tbbit):
-                            x[i][j][k][l] = -1 << xshift
-                        else:
-                            x[i][j][k][l] = np.round(celu_table[tb_idx] / (float) (1 << (celushift - xshift))).astype(x.dtype)
-
-        return x
 
 
 class ln():
